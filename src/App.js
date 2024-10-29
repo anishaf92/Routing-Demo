86bc3d3b71797e5e6ff17cbe7250c53552c4c9ef
Anisha Fathima <anishaf92@gmail.com>
2024-10-29 19:50:47 +0530

    Protected Route Added

--- src/App.js
+++ src/App.js
@@ -6,25 +6,39 @@ import {BrowserRouter, Link,Routes,Route} from "react-router-dom";
 import Login from './components/Login';
 import Blog from './components/Blog';
 import Post from './components/Post';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
+  const [isAuth,setIsAuth] = useState(false)
+  const navigate = useNavigate();
+  const onLogin = () => {
+    setIsAuth(true)
+    navigate('/dashboard')
+  }
+  const onLogout = () => {
+    setIsAuth(false)
+    navigate('/login')
+  }
   return (
-    <div className="App">
-      <BrowserRouter>
+    <div className="App">     
         <ul>
           <Link to='/'><li>Home</li></Link>
           <Link to='/login'><li>Login</li></Link>
           <Link to='/blog'><li>Blog Posts</li></Link>
           <Link to='/dashboard'><li>Dashboard</li></Link>
+          {isAuth ? <button onClick={onLogout}>Logout</button> : <></>}
         </ul>       
         <Routes>
           <Route path='/' element={<Home />}></Route>
           <Route path="/post/:num" element={<Post />} />
-          <Route path='/login' element={<Login />}></Route>
+          <Route path='/login' element={<Login onLogin={onLogin} />}></Route>
           <Route path='/blog' element={<Blog />}></Route>
-          <Route path='/dashboard' element={<Dashboard />}></Route>
+          <Route path='/dashboard' element={
+            <ProtectedRoute isAuth={isAuth}><Dashboard /></ProtectedRoute>}></Route>
           </Routes>
-      </BrowserRouter>      
+        
     </div>
 
   );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-function Login(){
-    return <div>This is a Login page</div>
+function Login({onLogin}){
+    
+    return (
+    <div>This is a Login page
+        <button onClick={onLogin}>Login</button>
+    </div>)
 }
 export default Login;
